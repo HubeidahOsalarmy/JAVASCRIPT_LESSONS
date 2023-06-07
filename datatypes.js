05dc6b952ffb27827a3b78230f56a070d9ee23de
HubeidahOsalarmy <133193592+HubeidahOsalarmy@users.noreply.github.com>
2023-06-08 00:21:09 +0100

    Add files via upload

---
+++ datatypes.js
@@ -0,0 +1,5 @@
+console.log(typeof("Hubeidah"))
+console.log(typeof(true))
+console.log(typeof(25))
+console.log(typeof())
+console.log(typeof(null))
---
+++ variable.js
@@ -0,0 +1,20 @@
+let firstName = "Hubeidah"
+let isMarried = true
+let schoolName
+let schoolLevel = null
+6. let firstName
+let last_name
+let isMarried
+let age */
+7.  let firstName = "Hubeidah"
+let last_name = "Salami"
+let age = 25
+let isMarried = true
+/*8.  let firstName = "Hubeidah"
+let last_name = "Salami"
+let age = 25
+let isMarried = true*/
+//9.  let firstName = "Hubeidah", last_name = "Salami", age = 25, isMarried = true//
+let myAge  = 25
+let yourAge = 30
+console.log("I am " + myAge + " " + "years old.\n" + "You" + " " + "are" + " " + yourAge + " " + "years old." )
